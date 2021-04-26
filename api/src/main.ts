a552c621515b1cf505676e69721f460fe36df519
Chase Check <marianqwerty511@gmail.com>
2021-04-26 11:17:51 +0300

    added new gitignore

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
+import * as fs from 'fs'
+
+const folders = ['/files','/files/images','/files/songs']
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  folders.forEach(f=>{
+    if(!fs.existsSync(f)){
+      fs.mkdirSync(f);
+    }
+  })
+  
   app.use(bodyParser.json({limit: '50mb'}));
   app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
   await app.listen(3000);
